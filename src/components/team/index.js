865fbd7f638dba629b37c24927ec5c8526c50118
Zach Wernberg <zawerny@gmail.com>
2018-08-27 09:07:45 -0500

    add owners

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -10,16 +10,19 @@ class Team extends Component {
     name() {
         return `${this.props.data.team.teamLocation} ${this.props.data.team.teamNickname}`.toString();
     }
+    owner() {
+        return `${this.props.data.team.owner}`.toString();
+    }
     render() {
         return (
             <Col xs={12} md={4} className= "team">
                 <Grid fluid>
                     <Row className="top">
                         <Col xs={2} md={2}> <Helmet /> </Col>
-                        <Col xs={10} md={10} className="team-name">{this.name()}</Col>
+                        <Col xs={10} md={10} className="team-name">{this.name()}  ({this.owner()})</Col>
                     </Row>
                     <Row className="bottom">
-                        <Col xs={6} md={6} className="total">Total</Col>
+                        <Col xs={6} md={6} className="total">TOTAL</Col>
                         <Col xs={6} md={6} className="points"><div className="points">{ this.props.data.score } </div></Col>
                     </Row>
                 </Grid>
