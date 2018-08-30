c8911d6d42fb0702875611820ef3a8b0fb66a310
Zach Wernberg <zawerny@gmail.com>
2018-08-30 16:25:06 -0500

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './components/app';
-import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
