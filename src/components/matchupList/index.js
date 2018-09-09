6bdedb8a0537c48d775006d607c51988f168b3c5
bwerny <benjamin.wernberg@gmail.com>
2018-09-09 12:55:10 -0400

    added header stars

--- src/components/matchupList/index.js
+++ src/components/matchupList/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Matchup  from '../matchup';
-import { Grid } from 'react-bootstrap';
+import { Row, Col, Grid } from 'react-bootstrap';
 import PropTypes from 'prop-types'
 
 import './style.css';
@@ -14,7 +14,13 @@ class MatchupList extends Component {
             <div className= 'matchupList'>
             { this.props.Matchups.divisions.map(league => (
                 <Grid fluid className="league" key={league.metadata.division}>
-                    <h2 className="name tecmo-underline">{league.metadata.division} DIVISION </h2>
+                    <Row className="division-header">
+                        <Col xs={0} md={3}></Col>
+                        <Col xs={2} md={1}><img src={"./stars.png"} alt="tecmo stars"/></Col>                      
+                        <Col xs={8} md={4}><h2 className="tecmo-underline">{league.metadata.division} DIVISION </h2></Col>
+                        <Col xs={2} md={1}><img src={"./stars.png"} alt="tecmo stars"/></Col>
+                        <Col xs={0} md={3}></Col>
+                    </Row>
                     { league.scoreboard.matchups.map(matchup => {
                         return <Matchup matchup={matchup} key={league.metadata.division + matchup.teams[0].teamId}/>
                     })}
